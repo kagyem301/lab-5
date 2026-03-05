a4343849ed760464379329c884d114e83449cfa3
kagyem301 <kagyem301@caledonian.ac.uk>
2026-03-05 12:11:49 +0000

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,11 @@
-my name is ramacita
+
 # This is a simple Python program
 
 # Your program begins with a call to main()
 # Prints "Hello, World" to the terminal window.
 def main():
-    print("Hello, World")
+    print("Hello, World ,how are you doing")
+    
 
 if __name__ == "__main__":
     main()
